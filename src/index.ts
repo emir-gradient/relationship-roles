08cb1de41436f0c2384b2245d54ce618e78223fa
Emir Alicic <emir@gradient.ba>
2022-10-13 15:52:44 +0200

    Authorization Service Factory exported from root index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 // istanbul ignore file
 export * from './authorization/relationship-authorization.service';
+export * from './authorization/authorization-service.factory';
 export * from './type/relationship-resolver';
 export * from './exception/supported-relationships-overlap.exception';
 export * from './relationship-resolver/relationship-resolver.container';
